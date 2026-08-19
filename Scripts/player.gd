cb5786412adf157edc340711697068917061ab19
joshuayeee <yjoshua430@gmail.com>
2026-08-19 02:37:58 -0700

    added player movement

---
+++ Scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+class_name Player
+
+const SPEED: float = 300.0
+
+func _physics_process(_delta: float) -> void:
+	var direction: float = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
