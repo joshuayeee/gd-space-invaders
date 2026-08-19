02a2db7f7e96b353f3caaa4bc91bd180615016f0
joshuayeee <yjoshua430@gmail.com>
2026-08-19 02:55:28 -0700

    player can now shoot bullets

---
+++ Scripts/bullet.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+class_name Bullet
+
+const SPEED: float = 300.0
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.UP * SPEED
+	move_and_slide()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 class_name Player
 
 const SPEED: float = 300.0
+const BULLET = preload("uid://cl3y7nhmch4ao")
+
+@export var level: Level = null
+@export var bullet_manager: BulletManager = null
 
 func _physics_process(_delta: float) -> void:
 	var direction: float = Input.get_axis("left", "right")
@@ -10,5 +14,10 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if (Input.is_action_just_pressed("shoot")):
+		var bullet: Bullet = BULLET.instantiate()
+		bullet.position = position
+		bullet_manager.add_child(bullet)
 	
 	move_and_slide()
